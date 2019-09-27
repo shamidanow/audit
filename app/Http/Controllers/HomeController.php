09983f2b2e2281df8d3cae3f9723da59d08d84b7
shamidanow <shamidanow@users.noreply.github.com>
2019-09-27 17:14:35 +0500

    api_id

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,25 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $client = new Client();
+        /*$res = $client->request('GET', 'https://api-sandbox.direct.yandex.com/json/v5/campaigns'
+            , [
+            'Authorization' => [
+                'Bearer' => auth()->user()->api_id
+            ]
+        ]);
+        $client->addHeader('Authorization', 'Bearer '.auth()->user()->api_id.'');*/
+        
+        $res = $client->get("https://api-sandbox.direct.yandex.com/json/v5/campaigns", [
+            'headers' => [
+                'Authorization' => 'Bearer '.auth()->user()->api_id
+            ],// здесь можно передать заголовки
+            'json' => [], // здесь можно передать тело запроса
+            'exceptions' =>false // эта опция отключит автоматическое выбрасывание исключений guzzle-ом
+        ]);
+        
+        $data = json_decode($res->getBody());
+        dd($data);
+        //return view('home');
     }
 }
